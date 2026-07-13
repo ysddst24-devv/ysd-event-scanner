79be30273db7911bbc29ea6acb006d679eaba7d0
ysddst24-devv <ysddst24@gmail.com>
2026-07-13 22:48:23 +0300

    Create config.js
    Config for cloudflare

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+    VERSION: "1.0.0",
+
+    EVENT: "IYD2026",
+
+    API_URL: "",
+
+    GATE: "Main Gate",
+
+    ENABLE_SOUND: true,
+
+    ENABLE_VIBRATION: true,
+
+    AUTO_RESET: 2500
+
+};
